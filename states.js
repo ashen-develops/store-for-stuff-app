da3e0dc14c5dea0c0aeb2c5301c409dd50d65c0b
Ashen Pennington <ashenmark@gmail.com>
2021-01-14 09:49:50 -0600

    init

---
+++ states.js
@@ -0,0 +1,104 @@
+
+   const allStates = [
+    {
+      id: '1',
+      state: 'Alabama',
+      city: 'Huntsville',
+      best-cheap-beer-spot: 'Voodoo Lounge',
+      bcbs-link: 'https://www.voodooloungehsv.com',
+      best-dog-friendly-spot: 'City of Huntsville Dogspot',
+      bdfs-link: 'https://www.huntsvilleal.gov/environment/animal-services/pet-services/dog-parks/',
+      best-outdoorsy-spot: 'Aldridge Creek Greenway',
+      bos-link: 'https://ourvalleyevents.com/8-favorite-places-for-free-fun-in-the-huntsville-outdoors/',
+      best-local-fast-food-spot: 'Shaggys Burgers and Tacos',
+      blffs-link: 'https://www.shaggysburgersandtacos.com'
+    },
+
+    {
+      id: '2',
+      state: 'Alabama',
+      city: 'Gulf Shores',
+      best-cheap-beer-spot: 'Big Beach Brewing Co.',
+      bcbs-link: 'https://www.bigbeachbrewing.com/copy-of-home',
+      best-dog-friendly-spot: 'Gulf Shores Dog Park',
+      bdfs-link: 'https://www.gulfshores.com/things-to-do/gulf-shores-dog-park/',
+      best-outdoorsy-spot: 'Gulf State Park',
+      bos-link: 'https://www.alapark.com/parks/gulf-state-park',
+      best-local-fast-food-spot: `Foosackly's`,
+      blffs-link: 'http://foosacklys.net'
+    },
+
+    {
+      id: '3',
+      state: 'Alaska',
+      city: 'Fairbanks',
+      best-cheap-beer-spot: 'The Boatel',
+      bcbs-link: 'https://www.facebook.com/akboatel/',
+      best-dog-friendly-spot: 'Fairbanks Dog Park',
+      bdfs-link: 'http://www.fairbanksdogpark.org',
+      best-outdoorsy-spot: 'Arctic National Wildlife Refuge',
+      bos-link: 'https://www.fws.gov/refuge/arctic/',
+      best-local-fast-food-spot: 'The Crepery',
+      blffs-link: 'https://www.thecrepery.net'
+    },
+
+    {
+      id: '4',
+      state: 'Alaska',
+      city: 'Juneau',
+      best-cheap-beer-spot: 'The Narrows Bar',
+      bcbs-link: 'https://www.facebook.com/pg/thenarrowsbaralaska/reviews/?ref=page_internal',
+      best-dog-friendly-spot: 'Mendenhall Wetlands State Game Refuge',
+      bdfs-link: 'http://www.adfg.alaska.gov/index.cfm?adfg=mendenhallwetlands.main',
+      best-outdoorsy-spot: 'Eagle Beach State Recreation Area',
+      bos-link: 'http://dnr.alaska.gov/parks/aspunits/southeast/eaglebeachsra.htm',
+      best-local-fast-food-spot: 'The Salmon Spot',
+      blffs-link: 'http://aksalmonspot.com'
+    },
+
+    {
+      id: '5',
+      state: 'Arizona',
+      city: 'Sedona',
+      best-cheap-beer-spot: 'Sedona Beer Company',
+      bcbs-link: 'https://www.sedonabeerco.com',
+      best-dog-friendly-spot: 'Sedona Dog Park',
+      bdfs-link: 'https://www.sedonaaz.gov/your-government/departments/parks-recreation/city-parks/dog-park',
+      best-outdoorsy-spot: 'Cathedral Rock Trail',
+      bos-link: 'Cathedral Rock Trail, Sedona, AZ 86351',
+      best-local-fast-food-spot: 'Tortas de Fuego',
+      blffs-link: 'https://www.tortasdefuegosedona.com'
+    },
+
+    {
+      id: '6',
+      state: 'Arizona',
+      city: 'Tuscon',
+      best-cheap-beer-spot: 'Arizona Beer House',
+      bcbs-link: 'https://www.arizonabeerhouse.com',
+      best-dog-friendly-spot: 'Mikos Corner Playground Dog Park',
+      bdfs-link: 'https://www.tucsonaz.gov/parks/dog-parks',
+      best-outdoorsy-spot: 'Brandi Fenton Memorial Park',
+      bos-link: 'https://webcms.pima.gov/cms/one.aspx?portalId=169&pageId=1367',
+      best-local-fast-food-spot: 'Jimmys Pita and Poke',
+      blffs-link: 'https://www.jimmys2go.com'
+    },
+
+    {
+      id: '7',
+      state: 'Arkansas',
+      city: 'Fayetville',
+      best-cheap-beer-spot: 'Arizona Beer House',
+      bcbs-link: 'https://www.arizonabeerhouse.com',
+      best-dog-friendly-spot: 'Mikos Corner Playground Dog Park',
+      bdfs-link: 'https://www.tucsonaz.gov/parks/dog-parks',
+      best-outdoorsy-spot: 'Brandi Fenton Memorial Park',
+      bos-link: 'https://webcms.pima.gov/cms/one.aspx?portalId=169&pageId=1367',
+      best-local-fast-food-spot: 'Jimmys Pita and Poke',
+      blffs-link: 'https://www.jimmys2go.com'
+    },
+
+  ];
+  
+  module.exports = cats;
+
